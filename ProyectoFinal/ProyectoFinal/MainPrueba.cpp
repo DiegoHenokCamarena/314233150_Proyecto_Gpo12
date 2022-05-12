c0a6663fbf26b22df30f39b40a994bb94e96ab18
diegoHCR <diego.camarena@ingenieria.unam.edu>
2022-05-12 09:51:20 -0500

    Entrega final

--- ProyectoFinal/ProyectoFinal/MainPrueba.cpp
+++ ProyectoFinal/ProyectoFinal/MainPrueba.cpp
@@ -203,7 +203,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Practica 12", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "314233150_ProyectoFinal", nullptr, nullptr);
 
 	if (nullptr == window)
 	{
